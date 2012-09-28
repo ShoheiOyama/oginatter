4bf79ce23775ced19e06a3cb0425f5c93e21cc0a
shoheioyama <smallfox27@gmail.com>
2012-09-29 03:21:34 +0900

    chart

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -18,6 +18,18 @@ class HomeController < ApplicationController
           (b[1] <=> a[1]) * 2 + (a[0] <=> b[0])
         }.slice(0,10)
       end
+      @end_at = Date.today
+      @start_at = @end_at - 6
+      @categories = @start_at.upto(@end_at).to_a
+      @data = [5, 6, 3, 1, 2, 4, 7]
+
+      @h = LazyHighCharts::HighChart.new("graph") do |f|
+        f.chart(:type => "column")
+        f.title(:text => "Sample graph")
+        f.xAxis(:categories => @categories)
+        f.series(:name => "sample",
+                 :data => @data)
+      end
     end
   end
 end
