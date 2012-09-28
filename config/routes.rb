bbe466ab90f7819acca4803b742e9259110b88aa
shoheioyama <smallfox27@gmail.com>
2012-09-29 01:32:34 +0900

    a

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Twitics::Application.routes.draw do
   resources :users
 
-  get "top/index"
+  root :to => "top#index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
